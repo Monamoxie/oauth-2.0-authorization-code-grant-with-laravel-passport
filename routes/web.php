b2644ab4a01d966dfa4fa8cd1170cb703fb87531
Mona Moxie <moxie4lyf@gmail.com>
2020-09-13 16:32:13 +0100

    update: web routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/developers', function () {
+    return view('developers');
 });
