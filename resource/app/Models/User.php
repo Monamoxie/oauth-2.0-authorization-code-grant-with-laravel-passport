b2d644e346a6909e504bb935ef83403ee82f03ed
Mona Moxie <moxie4lyf@gmail.com>
2020-09-16 10:42:21 +0100

    resource: use uuid trait and config defaults

--- resource/app/Models/User.php
+++ resource/app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\Uuids; 
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Uuids, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
---
+++ resource/app/Traits/Uuids.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait Uuids
+{
+    protected static function bootUsesUuid()
+    {
+        static::creating(function ($model) {
+            if (! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
