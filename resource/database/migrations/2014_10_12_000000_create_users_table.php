13d7999a089f7dab3bb53868bab16244da90ba26
Mona Moxie <moxie4lyf@gmail.com>
2020-09-16 11:55:08 +0100

    resource: installed livewire

--- resource/database/migrations/2014_10_12_000000_create_users_table.php
+++ resource/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
