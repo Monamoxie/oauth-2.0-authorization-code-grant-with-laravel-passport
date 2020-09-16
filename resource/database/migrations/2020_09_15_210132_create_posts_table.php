465a2962d6d051b1634696ab10c09673654ce6e2
Mona Moxie <moxie4lyf@gmail.com>
2020-09-16 10:48:19 +0100

    resource: install passport

--- resource/database/migrations/2020_09_15_210132_create_posts_table.php
+++ resource/database/migrations/2020_09_15_210132_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id')->unsigned();
+            $table->uuid('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title');
             $table->text('content');
